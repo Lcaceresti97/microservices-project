e29c593c7e5519b722a9dfe3f4ce52fbda01c848
Lcaceresti97 <laurentcaceresti@gmail.com>
2022-04-28 15:48:09 -0600

    multiple-instances

--- config-service/src/main/java/com/sti/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/sti/configservice/ConfigServiceApplication.java
@@ -1,6 +1,6 @@
 package com.sti.configservice;
 
-import |org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
